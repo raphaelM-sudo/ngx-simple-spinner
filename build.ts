ee798bc313eceb24ce230ddabd9aa9c0eeff8c93
Raphael M <raphael.michl@protonmail.com>
2020-06-20 16:40:04 +0200

    Created project base

---
+++ build.ts
@@ -0,0 +1,66 @@
+import * as del from 'del';
+import * as fs from 'fs';
+import { copySync } from 'fs-extra';
+import { ngPackagr } from 'ng-packagr';
+import * as sass from 'node-sass';
+import { join } from 'path';
+
+async function main() {
+  // cleanup dist
+  try {
+    del.sync(join(process.cwd(), '/dist'));
+    del.sync(join(process.cwd(), '/node_modules/@nutrify/ngx-simple-spinner'));
+  } catch (e) {
+    // tslint:disable-next-line: no-console
+    console.info('Ignoring: Could not delete previous build.');
+  }
+
+  await ngPackagr()
+    .forProject(join(process.cwd(), 'projects/simple-spinner/package.json'))
+    .build();
+
+  // put it in node modules so the path resolves
+  // proper path support eventually
+  copySync(
+    join(process.cwd(), 'README.md'),
+    join(process.cwd(), '/dist/simple-spinner/README.md')
+  );
+  copySync(
+    join(process.cwd(), 'LICENSE'),
+    join(process.cwd(), '/dist/simple-spinner/LICENSE')
+  );
+  copySync(
+    join(process.cwd(), 'projects/simple-spinner/src/lib/styles.scss'),
+    join(process.cwd(), '/dist/simple-spinner/scss/styles.scss')
+  );
+
+  // Copy SCSS and create CSS file
+  const cssDir = join(process.cwd(), '/dist/simple-spinner/css');
+  const scssFile = join(process.cwd(), 'projects/simple-spinner/src/lib/styles.scss');
+  const cssOutFile = cssDir + '/styles.css';
+
+  if (!fs.existsSync(cssDir)) {
+    fs.mkdirSync(cssDir);
+  }
+
+  sass.render({
+    file: scssFile,
+    outFile: cssOutFile
+  }, (error: any, result: any) => {
+    if (!error) {
+      fs.writeFile(cssOutFile, result.css, () => {});
+    }
+  });
+
+  copySync(
+    join(process.cwd(), '/dist/simple-spinner'),
+    join(process.cwd(), '/node_modules/@nutrify/ngx-simple-spinner')
+  );
+}
+
+main()
+  .then(() => console.log('success'))
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'simple-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ngx-simple-spinner';
+  version = VERSION.full;
+
+  ngOnInit() {
+    // tslint:disable-next-line: no-console
+    console.info(
+      'I am developing those modules on my own, in my free time. ' +
+      'It is very time consuming to deliver quality code.\n' +
+      '\nIf you appreciate my work, please buy me a coffee 😊\n' +
+      '\nThanks'
+    );
+  }
 }
